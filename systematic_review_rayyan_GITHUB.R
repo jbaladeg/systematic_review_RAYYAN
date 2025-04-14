69797dd75e95a2653b3f5b40cf32745e376c6d89
Jenny <100955286+jbaladeg@users.noreply.github.com>
2025-04-14 10:52:02 +0100

    Update systematic_review_rayyan_GITHUB.R

--- systematic_review_rayyan_GITHUB.R
+++ systematic_review_rayyan_GITHUB.R
@@ -1,6 +1,6 @@
-#ICC para tres jueces en una revision sistematica por bloques (script)
+#ICC for three judges in a systematic block review (script)
 
-#0. carga librerias necesarias
+#0. load required libraries
 
 library(tidyr)
 library(dplyr)
@@ -10,21 +10,21 @@ library(irr)
 library(stringr)
 
 
-#BLOQUE 1----
-#1.abre directorio (Control + Mayus + H)
-setwd("~/JENNY/Docencia ULL/TFGs/2025/grupo kimberly, carla y laura") #este es para mi caso, pero el vuestro ser? otro
-
-#2. busca el archivo y lee la base
+#BLOQUE 1 / BLOCK 1----
+#1.Open directory (Control + Mayus + H)
+#2. find the file and read the base
 dir()
 bd <- as.data.frame(read.csv('articles_github.csv'))
 
 View(bd)
-#3. preparo la base
+#3. Prepare the base
 
-bd <- bd[,c("key","notes")] #me quedo solo con las columnas que me interesa
+bd <- bd[,c("key","notes")] #I just want to use these variables
 
+#####this is a extra step... if you want to simplify the script, just change the names of your raters with "rater1", "rater2"...
+#####In this case, "articles_github_cvs already has this change. Don't run this part for the example.
 bd <- bd %>%
-  mutate(notes2 = notes %>%
+  mutate(notes = notes %>%
            str_replace_all("name_of_your_rater_1", "rater1") %>%
            str_replace_all("name_of_your_rater_2", "rater2") %>%
            str_replace_all("name_of_your_rater_3", "rater3"))
@@ -32,16 +32,16 @@ bd <- bd %>%
 names(bd)
 
 bd <- bd %>%
-  mutate(rayyan_inclusion = str_extract(notes, "RAYYAN-INCLUSION: \\{[^}]+\\}")) #separo las inclusiones
+  mutate(rayyan_inclusion = str_extract(notes, "RAYYAN-INCLUSION: \\{[^}]+\\}")) #separate decision column
 bd <- bd %>%
-  mutate(bloques = str_extract(notes, "bloque 1")) #separo las etiquetas de los bloques
+  mutate(bloques = str_extract(notes, "bloque 1")) #separate the labels from the blocks
 
-#4 preparo las variables de decision
+#4. Prepare decision labels
 
 extraer_decision <- function(texto, nombre) {
-  # Si el texto es NA, devolvemos NA directamente
+  # If the value is NA, return NA
   if (is.na(texto)) return(NA)
-  # Usamos expresi?n regular para extraer el valor asociado al nombre
+  # We use regular expression to extract the value associated with the name
   patron <- paste0('"', nombre, '"=>\\"(.*?)\\"')
   resultado <- regmatches(texto, regexpr(patron, texto, perl = TRUE))
   if (length(resultado) == 0) return(NA)
@@ -53,41 +53,39 @@ bd$rater2 <- sapply(bd$rayyan_inclusion, extraer_decision, nombre = "rater2")
 bd$rater3    <- sapply(bd$rayyan_inclusion, extraer_decision, nombre = "rater3")
 
 nv(bd)
-bd[,5:7]#compruebo
+bd[,5:7]#check
 
-#5. cambio los included por 1 y los excluded por 0
-
+#5. I change the included by 1 and the excluded by 0
 bd$rater1_n    <- ifelse(is.na(bd$rater1), NA, ifelse(bd$rater1 == "Included", 1, 0))
 bd$rater2_n <- ifelse(is.na(bd$rater2), NA, ifelse(bd$rater2 == "Included", 1, 0))
 bd$rater3_n    <- ifelse(is.na(bd$rater3), NA, ifelse(bd$rater3 == "Included", 1, 0))
 
 
-bd[,4:10]#compruebo
+bd[,4:10]#check
 
+#6. filter per block 1
 
-#6. filtro por bloque 1
-
-bd_filtrada <- subset(bd, bloques == "bloque 1")
+bd_filter <- subset(bd, blocks == "bloque 1")
 
 #7. ICC bloque 1
 
-with(bd_filtrada, table(rater1_n, rater2_n, rater3_n))
-
-#forma 1 = ICC
-decisiones <- bd_filtrada[, c("rater1_n", "rater2_n", "rater3_n")]
-print(icc(decisiones, model = "twoway", type = "consistency", unit = "single"))
-#resultado = .23
+with(bd_filter, table(rater1_n, rater2_n, rater3_n))
 
 
-#forma 2 = Fleiss de kappa
-irr::kappam.fleiss(decisiones)
-#resultado = .21
+#form 1 = ICC (in general)
+decision <- bd_filter[, c("rater1_n", "rater2_n", "rater3_n")]
+print(icc(decision, model = "twoway", type = "consistency", unit = "single"))
+    #result = .23
 
+#form 2 = Fleiss de kappa (for three raters)
+decision <- bd_filter[, c("rater1_n", "rater2_n", "rater3_n")]
+irr::kappam.fleiss(decision)
+    #result = .21
 
-#Interpretación
-# Rango ICC	  Nivel de acuerdo
+#Interpretation
+# ICC rate 	  Agreement
 #
-#   <0.40	    Pobre
-#0.40 - 0.59	Moderado
-#0.60 - 0.74	Bueno
-#0.75 - 1.00	Excelente
+#   <0.40	    Poor
+#0.40 - 0.59	Moderate
+#0.60 - 0.74	Good
+#0.75 - 1.00	Excelnt
